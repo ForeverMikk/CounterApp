1e8aa17490702fd2cdece486ed67c1e9ec2c2540
Michael Molina <michaelmolina679@gmail.com>
2021-02-09 19:02:26 -0600

    Building a new counter app to learning React

---
+++ src/CounterApp.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+    //handleAdd
+
+    const handleAdd = (e) => {
+        console.log(e)
+    } 
+    
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <p>{value}</p>
+
+            <button onClick={ handleAdd }>+1</button>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+
+export default CounterApp;
